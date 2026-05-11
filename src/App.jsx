159456e055377c1a587a76778bb6f4e4e5fec221
Lubeth <lubethjimenez@yahoo.com>
2026-05-11 19:57:51 -0400

    Add GET/READ table from Database w/ Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 
+import { useEffect, useState } from 'react'
+import { supabase } from './utils/supabase'
 import './App.css'
 
 function App() {
-     
+     const [movies, setMovies] = useState([]);
+
+     useEffect(()=>{
+      async function getMovies() {
+        try {
+          const { data } = await supabase.from("movies").select("*");
+          //console.log(data)
+          setMovies(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      getMovies();
+     })
 
   return (
-    <div>
+    <div className="app">
       <h1>CRUD (Movies Table)</h1>
+      <h2>Movie List</h2>
     </div>
   )
 }
