acd5da5ad88290ec3ce86d94bb11c56666e9858c
Lubeth <lubethjimenez@yahoo.com>
2026-05-11 20:51:03 -0400

    Add Pagination component w/ CSS & created prev/next button functions to load Data on pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import { useEffect, useState } from 'react'
 import { supabase } from './utils/supabase'
 import './App.css'
 import MovieTable from './components/MovieTable';
+import Pagination from './components/Pagination';
 
 function App() {
      const [movies, setMovies] = useState([]);
+//Pagination State//
+     const [page, setPage] = useState(0);
+     const [numberOfPages, setNumberOfPages] = useState(0);
+     const pageSize = 10;
 
      useEffect(()=>{
       async function getMovies() {
         try {
-          const { data } = await supabase.from("movies").select("*");
+          const from = page * pageSize;
+          const to = from + pageSize - 1;
+
+          const { data, count } = await supabase.from("movies").select("*", { count: "exact" }).range(from, to);
           //console.log(data)
+          setNumberOfPages(Math.ceil(count / pageSize) - 1);
           setMovies(data);
         } catch (error) {
           console.log(error);
@@ -19,12 +28,14 @@ function App() {
       }
       getMovies();
      })
+     //console.log(numberOfPages);
 
   return (
     <div className="app">
       <h1>CRUD (Movies Table)</h1>
       <h2>Movie List</h2>
       <MovieTable movies={movies} />
+      <Pagination setPage={setPage} page={page} numberOfPages={numberOfPages} />
     </div>
   )
 }
